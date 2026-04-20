9602a5637e857d703fbec7d226eb99834c41c054
Tiffany24630 <sal24630@uvg.edu.gt>
2026-04-19 23:41:43 -0600

    Correcciones

--- app/models/permissions.py
+++ app/models/permissions.py
@@ -2,8 +2,8 @@ from sqlalchemy import Column, Integer, String
 from app.core.database import Base
 
 class Permission(Base):
-    _tablename_ = "permissions"
-    _table_args_ = {"schema": "global"}
+    __tablename__ = "permissions"
+    __table_args__ = {"schema": "global"}
 
     id = Column(Integer, primary_key=True)
     name = Column(String(100), unique=True, nullable=False)
